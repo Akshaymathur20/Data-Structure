9b85f3b84d15aa913313e52e765de4e3e6e2df4a
Akshay Mathur <akshaymathur242@gmail.com>
2022-12-05 01:53:14 +0530

    rist

---
+++ DataStructure/Searching/Binary Search/BinarySearch.java
@@ -0,0 +1,9 @@
+package DataStructure.Searching.BinarySearch;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        // int arr[] = { 8,9,6,5, 4};
+        System.out.println("Binary Search"); 
+    }
+    
+}
