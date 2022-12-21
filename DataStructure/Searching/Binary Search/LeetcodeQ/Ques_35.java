6d041d0a02f5f6637734eba9beeead0dcf03e83e
Akshay Mathur <akshaymathur242@gmail.com>
2022-12-21 22:37:05 +0530

    second

---
+++ DataStructure/Searching/Binary Search/LeetcodeQ/Ques_35.java
@@ -0,0 +1,21 @@
+// package Searching.Binary Search.LeetcodeQ;
+
+public class Ques_35 {
+    public int searchInsert(int[] nums, int target) {
+        
+        int start = 0;
+        int  end = nums.length-1;
+        while(start<=end){
+           int mid = (start + end)/2;
+            if(target<nums[mid]){
+                end = mid-1;
+            }else if(target>nums[mid]){
+                start = mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return start;
+    }
+    
+}
