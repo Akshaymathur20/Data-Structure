95868836d4128f14dc411f037934a6052864aba1
Akshay Mathur <akshaymathur242@gmail.com>
2022-12-24 23:54:12 +0530

    first

---
+++ DataStructure/String/startString.java
@@ -0,0 +1,22 @@
+// package String;
+
+public class startString {
+    public static void main(String[] args) {
+            // int a = 10;
+    
+            String name  = "Akshay";
+        
+    
+            //String are immutable we cannot change it by another referenc object;
+    
+            //here below I did not change the object here I created a new object,(Think once again)
+        
+            String a = "Akshay ";
+            System.out.println(a);
+            a="Mathur";
+            System.out.println(a);
+            // String b = "Akshay";
+    }
+       
+    }
+    
