8cd51fe204039681971ad33f6b68f0d64b895273
Akshay Mathur <akshaymathur242@gmail.com>
2022-12-27 22:01:05 +0530

    second

--- DataStructure/Recursion/RecMessage2.java
+++ DataStructure/Recursion/RecMessage2.java
@@ -2,26 +2,41 @@ public class RecMessage2 {
     public static void main(String[] args) {
         //write a functino that takes in a nubmer and prints it
         //print first 5 number : 1 2 3 4 5
-        print1(1);
+    //     print1(1);
+         print(1);
     }
-    static void print1(int n){
+
+    // static void print1(int n){
+    //     System.out.println(n);
+    //     print2(2);
+    // }
+    // static void print2(int n){
+    //     System.out.println(n);
+    //     print3(3);
+    // }
+    // static void print3(int n){
+    //     System.out.println(n);
+    //     print4(4);
+    // }
+    // static void print4(int n){
+    //     System.out.println(n);
+    //     print5(5);
+    // }
+    // static void print5(int n){
+    //     System.out.println(n);
+    // }
+
+
+
+    //Recursion  
+    
+    static void print(int n){
+        if(n==5){             //base condition to  stop
+            System.out.println(5);
+            return;
+        }
         System.out.println(n);
-        print2(2);
-    }
-    static void print2(int n){
-        System.out.println(n);
-        print3(3);
-    }
-    static void print3(int n){
-        System.out.println(n);
-        print4(4);
-    }
-    static void print4(int n){
-        System.out.println(n);
-        print5(5);
-    }
-    static void print5(int n){
-        System.out.println(n);
+        print(n+1);   //recursive call
     }
     
 }
