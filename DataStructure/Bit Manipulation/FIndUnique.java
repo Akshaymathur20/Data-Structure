f30e0019bd2beacef9cbb0ac30eb3adc20941875
Akshay Mathur <akshaymathur242@gmail.com>
2022-12-31 00:05:23 +0530

    first

---
+++ DataStructure/Bit Manipulation/FIndUnique.java
@@ -0,0 +1,22 @@
+// package Bit Manipulation;
+
+public class FIndUnique {
+    public static void main(String[] args) {
+        int [] arr  = {2,3,4,1,2,1,3,6,4};
+        System.out.println(Unique(arr));
+
+
+    }
+
+    static int Unique(int[]arr){
+
+        int unique = 0;
+
+        for(int n:arr){
+            unique= unique ^n;
+        }
+        return unique;
+
+    }
+    // Timc Complexity -- O(n);
+}
