4fa0e876ffe9a94a132f26fa55ad42bc2bd0d69d
Akshay Mathur <akshaymathur242@gmail.com>
2023-01-06 23:52:37 +0530

    first

--- DataStructure/Bit Manipulation/Problems/Leetcode.java
+++ DataStructure/Bit Manipulation/Problems/Leetcode.java
@@ -1,14 +1,15 @@
 public class Leetcode {
     public static void main(String[] args) {
-        int [] nums = {2,2,1};
+        int [] nums = {2,2,1,4 ,4};
+        // int [] nums = {2,2,1};
         System.out.println(singleNumber(nums));
         
     }
     
     // use the below code for leetcode solution just make in public 
      static int singleNumber(int[] num) {
-          
-         int n= 0;
+          int n= 0;
+
         for(int i :num){
             n = n^i;
         }
